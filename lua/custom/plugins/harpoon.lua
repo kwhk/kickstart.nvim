9a82bc5ce1241e0c87a7ac4626b8e9158593e0ae
Kevin Kim <kwhk@amazon.com>
2025-03-24 13:09:14 +0000

    Save a buffer into a specific index for Harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,6 +9,12 @@ return {
         harpoon:setup()
 	vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = '[H]arpoon [A]dd'})
 	vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon [M]enu' })
+	vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end, { desc = '[H]arpoon [C]lear' })
+
+	vim.keymap.set("n", "<leader>h<C-h>", function() harpoon:list():replace_at(1) end, { desc = 'Add to first Harpoon' })
+	vim.keymap.set("n", "<leader>h<C-j>", function() harpoon:list():replace_at(2) end, { desc = 'Add to second Harpoon' })
+	vim.keymap.set("n", "<leader>h<C-k>", function() harpoon:list():replace_at(3) end, { desc = 'Add to third Harpoon' })
+	vim.keymap.set("n", "<leader>h<C-l>", function() harpoon:list():replace_at(4) end, { desc = 'Add to fourth Harpoon' })
 
 	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = 'Focus on first Harpoon' })
 	vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = 'Focus on second Harpoon' })
