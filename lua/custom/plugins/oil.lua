eb91ff0120af891a812aacafc276becf989504f3
Kevin Kim <kwhk@amazon.com>
2025-04-05 01:49:31 +0000

    add oil plugin

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  dependencies = {
+    {
+      'echasnovski/mini.icons',
+      opts = {}
+    }
+  },
+  config = function ()
+    require('oil').setup({
+      keymaps = {
+        -- Below are the default keybindings that exist within an Oil buffer
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      }
+    })
+
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  lazy = false,
+}
